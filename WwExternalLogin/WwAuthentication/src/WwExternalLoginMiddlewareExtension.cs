20d527559b75132da96d51de0085ace23caea669
Charlie <stg609@outlook.com>
2021-01-11 21:50:28 +0800

    允许自定义 Displayname

--- WwExternalLogin/WwAuthentication/src/WwExternalLoginMiddlewareExtension.cs
+++ WwExternalLogin/WwAuthentication/src/WwExternalLoginMiddlewareExtension.cs
@@ -16,5 +16,15 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <returns></returns>
         public static AuthenticationBuilder AddWw(this AuthenticationBuilder builder, Action<WwOptions> configureOptions) =>
             builder.AddOAuth<WwOptions, WwHandler>("Ww", "Ww", configureOptions);
+
+        /// <summary>
+        /// 提供微信企业版外部登陆
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="displayName">显示的名称</param>
+        /// <param name="configureOptions"></param>
+        /// <returns></returns>
+        public static AuthenticationBuilder AddWw(this AuthenticationBuilder builder, string displayName, Action<WwOptions> configureOptions) =>
+            builder.AddOAuth<WwOptions, WwHandler>("Ww", displayName, configureOptions);
     }
 }
